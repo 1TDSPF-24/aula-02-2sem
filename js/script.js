72f39720d0cc21586f2b55c6be2727c05c8f7c44
gudeaguiar <gudeaguiar@gmail.com>
2024-08-07 12:13:43 -0300

    aula 07/08

---
+++ js/script.js
@@ -0,0 +1,65 @@
+
+
+
+// //Declarando um array de frutas:
+// let frutas = [ "Banana", "Limão","Goiaba","Melancia","Uva"];
+// const saidaDoMap = frutas.map( (f)=>(f));
+// console.log(saidaDoMap);
+
+//Funções mais simples podem ser utilizadas com ()!
+
+// let frutas = [ "Banana", "Limão","Goiaba","Melancia","Uva"];
+
+// const arrayObjFrutas = frutas.map( (fruta,indice)=>{
+//     return {"id":(indice+1),"nome":fruta};
+// });
+
+// console.log(arrayObjFrutas);
+
+// const arrayObjFrutasImpares = arrayObjFrutas.map((fruta)=>{
+//     const frutaImpar = arrayObjFrutas.find((f)=> (f.id === fruta.id && f.id % 2 != 0));
+//     return frutaImpar;
+// });
+
+// console.log(arrayObjFrutasImpares);
+
+// const arrayObjFrutasPares = arrayObjFrutas.filter( (fruta)=>(fruta.id % 2 == 0));
+// console.log(arrayObjFrutasPares);
+
+
+//Declarando um array de frutas:
+// let frutas = [ "Banana", "Limão","Goiaba","Melancia","Uva"];
+
+// //Declarando um array de preço de frutas:
+// let preços = [ 8.90,  12.88, 20.34, 33.40, 15.40];
+
+// const frutasPrecificadas = frutas.map((fruta,indice)=>{
+
+//         //Mecanismo de seleção de objetos com a condição desejada;
+//         const preco = precos.find((p,i)=> i == indice);
+
+//         //Montando o objeto
+//         const objFruta = {"id":(indice+1),"nome":fruta,"preco":preco}
+//         return objFruta;
+// });
+
+// console.log(frutasPrecificadas)
+
+
+// Ternário exemplo:
+
+// let nr1 =  10;
+// let nr2 =  5;
+// let resultado;
+
+// resultado = nr1 > nr2 ? "É maior": "É menor";
+// console.log(resultado);
+
+
+
+
+
+
+
+
+
